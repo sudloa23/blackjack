c393003c44e2b74e89c12f9bf1e33f55bfd211fe
sudloa23 <sudloa23@htl-kaindorf.at>
2026-04-04 13:01:03 +0200

    removed Deck class -> moved deck init to Game

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -4,14 +4,20 @@ import java.util.List;
 
 public class Game{
     private int playerCardX = 200, playerCardY = 200;
-    private Deck deck = new Deck();
     private List<Card> cards = new ArrayList<>();
     private List<Card> player = new ArrayList<>();
     private int random;
     private boolean draw = true;
+    private String[] symbols = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    private String heart = "\u2665", diamond = "\u2666", spade = "\u2660", club = "\u2663";
 
     public Game(){
-        cards = deck.getCards();
+        for(int i = 0; i < symbols.length; i++){
+            cards.add(new Card(heart, symbols[i]));
+            cards.add(new Card(diamond, symbols[i]));
+            cards.add(new Card(spade, symbols[i]));
+            cards.add(new Card(club, symbols[i]));
+        }
         update();
     }
 
