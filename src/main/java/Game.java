5853baf0461c36a104f386d6ab9c2dc434f908a6
sudloa23 <sudloa23@htl-kaindorf.at>
2026-04-04 14:51:49 +0200

    added drawBack method

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -6,10 +7,14 @@ public class Game{
     private int playerCardY = 300;
     private List<Card> cards = new ArrayList<>();
     private List<Card> player = new ArrayList<>();
+    private List<Card> bot = new ArrayList<>();
     private int random;
     private boolean draw = true;
     private String[] symbols = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
     private String heart = "\u2665", diamond = "\u2666", spade = "\u2660", club = "\u2663";
+    private int width = 80, height = 140, arcSize = 14;
+    private String filledBox = "\u25A3", emptyBox = "\u25A2";
+    private int boxSize = 10;
 
     public Game(){
         for(int i = 0; i < symbols.length; i++){
@@ -24,6 +29,7 @@ public class Game{
     public void init(){
         for(int i = 0; i < 2; i++){
             player.add(drawCard());
+            bot.add(drawCard());
         }
     }
 
@@ -31,6 +37,8 @@ public class Game{
         for(int i = 0; i < player.size(); i++){
             player.get(i).draw(g2d, i*100 + 15, playerCardY);
         }
+
+        drawBack(g2d,210, 100);
     }
 
     public Card drawCard(){
@@ -38,5 +46,28 @@ public class Game{
         Card temp = cards.get(random);
         cards.remove(random);
         return temp;
+    }
+
+    public void drawBack(Graphics2D g2d, int x, int y){
+        Font DEFAULT = g2d.getFont();
+        Color color = new Color(61, 68, 104);
+        AffineTransform original = g2d.getTransform();
+        g2d.drawRoundRect(x,y,width,height,arcSize,arcSize);
+        g2d.setColor(color);
+        g2d.fillRoundRect(x, y, width, height, arcSize, arcSize);
+        g2d.setColor(new Color(193, 196, 218));
+        g2d.setFont(new Font("Serif", Font.BOLD, boxSize));
+
+
+        for(int i = 0; i < 140/boxSize; i++){
+            for(int j = 0; j < 80/boxSize; j++){
+                if((i + j)%2 == 0){
+                    g2d.drawString(filledBox, x + (j*boxSize) + 1, y + ( i*boxSize) + boxSize-1);
+                }else {
+                    g2d.drawString(emptyBox, x + (j*boxSize) + 1, y + ( i*boxSize) + boxSize-1);
+                }
+            }
+        }
+
     }
 }
